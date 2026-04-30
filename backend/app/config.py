2966dcaa2ea764821cf69caec0da4530a9985bf2
excusasclub <elclubdelaexcusa@gmail.com>
2026-04-30 09:47:45 +0200

    Fix: load_dotenv sin override para respetar variables de entorno de Docker

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import os
 
 ROOT_DIR = Path(__file__).parent.parent
-load_dotenv(ROOT_DIR / '.env', override=True)
+load_dotenv(ROOT_DIR / '.env')
 
 MONGO_URL = os.environ['MONGO_URL']
 DB_NAME = os.environ['DB_NAME']
